6ec0ad97bf72c39036d046b441127a544c5e16ed
Hannesandd <157639794+Hannesandd@users.noreply.github.com>
2024-01-26 21:05:43 +0800

    Add files via upload

---
+++ Parlor_Services/src/parlor_services/PSS.java
@@ -0,0 +1,14 @@
+
+package parlor_services;
+
+public class PSS {
+    
+    public static void main(String[] args) {
+        
+        //initiating the first frame for to program to start
+        Start_Window frameselection = new Start_Window();
+        frameselection.setVisible(true);
+        frameselection.setLocationRelativeTo(null); 
+    }
+    
+}
